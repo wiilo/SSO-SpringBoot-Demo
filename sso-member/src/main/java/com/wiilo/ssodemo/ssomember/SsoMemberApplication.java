31dc1c5e956cbe1f0b2db7d774c498123e80a4f3
whitlock.wang <whitlock.wang@gshopper.com>
2022-08-31 16:36:02 +0800

    整合swagger2和log4j2

--- sso-member/src/main/java/com/wiilo/ssodemo/ssomember/SsoMemberApplication.java
+++ sso-member/src/main/java/com/wiilo/ssodemo/ssomember/SsoMemberApplication.java
@@ -1,5 +1,6 @@
 package com.wiilo.ssodemo.ssomember;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.wiilo.ssodemo.ssomember.feign")
 @ComponentScan(basePackages = {"com.wiilo.ssodemo", "com.wiilo.common"})
+@MapperScan("com.wiilo.ssodemo.ssomember.dao")
 public class SsoMemberApplication {
 
     public static void main(String[] args) {
---
+++ sso-member/src/main/java/com/wiilo/ssodemo/ssomember/dao/TestMapper.java
@@ -0,0 +1,7 @@
+package com.wiilo.ssodemo.ssomember.dao;
+
+/**
+ * @author Whitlock Wang
+ */
+public interface TestMapper {
+}
